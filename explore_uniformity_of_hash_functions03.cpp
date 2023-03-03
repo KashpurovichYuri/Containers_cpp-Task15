82b08de265fae2fc54d2301db256922f6fcef858
Yuri Kashpurovich <yurikashpurovich@gmail.com>
2023-03-03 20:46:54 +0300

    Added the second and the third subtasks.

---
+++ explore_uniformity_of_hash_functions03.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <set>
+#include <string>
+#include <array>
+#include <functional>
+#include <unordered_set>
+#include "Hash Functions/Hash Functions.cpp"
+
+std::set < std::string > make_random_words(std::size_t N, std::size_t length) // length = 10 - good enough
+{
+	std::uniform_int_distribution <> letter(97, 122);
+	std::default_random_engine e(static_cast <std::size_t> (
+		std::chrono::system_clock::now().time_since_epoch().count()));
+
+	std::set < std::string > words;
+
+	for (std::string s(length, '_'); std::size(words) < N; words.insert(s))
+		for (auto& c : s)
+			c = letter(e);
+
+	return words;
+}
+
+template < 	unsigned int (*Function)(const char*, unsigned int) >
+auto calculation_of_number_of_collisions(const std::set< std::string >& set_of_string, std::vector< std::vector< int> >& points)
+{
+	std::set < std::size_t > collisions;
+	auto num_of_collisions = 0, i = 0;
+	for (const auto& string : set_of_string)
+	{
+		auto pair = collisions.insert(Function(string.c_str(), string.size()));
+		auto flag = pair.second;
+		if (!flag)
+			num_of_collisions++;
+
+		std::vector<int> point{i, num_of_collisions};
+		points.push_back(point);
+		i++;
+
+		if (!flag)
+			std::cout << point[0] << " " << point[1] << std::endl;
+	}
+}
+
+int main()
+{
+	const std::size_t N = 10000000;
+	const std::size_t length = 6;
+	std::set< std::string > set_of_string = make_random_words(N, length);
+
+	//std::array< std::array< std::size_t, 2 >, N > pointsRS;
+	//=> if I will do all for 2D-std::array it won't work but why? => Because there is a question about the size of set_of_strings
+	
+	/*std::cout << "RS Hash Function" << std::endl;
+	std::vector < std::vector <int> > pointsRS;
+	calculation_of_number_of_collisions < RSHash > (set_of_string, pointsRS);
+	std::cout << std::endl;
+
+	std::cout << "JS Hash Function" << std::endl;
+	std::vector < std::vector <int> > pointsJS;
+	calculation_of_number_of_collisions < JSHash >(set_of_string, pointsJS);
+	std::cout << std::endl;
+
+	std::cout << "PJW Hash Function" << std::endl;
+	std::vector < std::vector <int> > pointsPJW;
+	calculation_of_number_of_collisions < PJWHash >(set_of_string, pointsPJW);
+	std::cout << std::endl;
+
+	std::cout << "ELF Hash Function" << std::endl;
+	std::vector < std::vector <int> > pointsELF;
+	calculation_of_number_of_collisions < ELFHash >(set_of_string, pointsELF);
+	std::cout << std::endl;
+
+	std::cout << "BKDR Hash Function" << std::endl;
+	std::vector < std::vector <int> > pointsBKDR;
+	calculation_of_number_of_collisions < BKDRHash >(set_of_string, pointsBKDR);
+	std::cout << std::endl;
+	
+	std::cout << "SDBM Hash Function" << std::endl;
+	std::vector < std::vector <int> > pointsSDBM;
+	calculation_of_number_of_collisions < SDBMHash >(set_of_string, pointsSDBM);
+	std::cout << std::endl;
+	
+	std::cout << "DJB Hash Function" << std::endl;
+	std::vector < std::vector <int> > pointsDJB;
+	calculation_of_number_of_collisions < DJBHash >(set_of_string, pointsDJB);
+	std::cout << std::endl;
+	*/
+	std::cout << "DEK Hash Function" << std::endl;
+	std::vector < std::vector <int> > pointsDEK;
+	calculation_of_number_of_collisions < DEKHash >(set_of_string, pointsDEK);
+	std::cout << std::endl;
+	/*
+	std::cout << "AP Hash Function" << std::endl;
+	std::vector < std::vector <int> > pointsAP;
+	calculation_of_number_of_collisions < APHash >(set_of_string, pointsAP);
+	std::cout << std::endl;*/
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
---
+++ uniformity_of_hash02.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <set>
+#include <string>
+#include <array>
+#include <functional>
+#include <unordered_set>
+
+std::set < std::string > make_random_words(std::size_t N, std::size_t length) // length = 10 - good enough
+{
+	std::uniform_int_distribution <> letter(97, 122);
+	std::default_random_engine e(static_cast <std::size_t> (
+		std::chrono::system_clock::now().time_since_epoch().count()));
+
+	std::set < std::string > words;
+
+	for (std::string s(length, '_'); std::size(words) < N; words.insert(s))
+		for (auto& c : s)
+			c = letter(e);
+
+	return words;
+}
+
+// http://www.boost.org/doc/libs/1_35_0/doc/html/hash/combine.html
+
+template < typename T >
+void hash_combine(std::size_t& seed, const T& value) noexcept
+{
+	seed ^= std::hash < T >()(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+template < typename T >
+void hash_value(std::size_t& seed, const T& value) noexcept
+{
+	hash_combine(seed, value);
+}
+
+template < typename T, typename ... Types >
+void hash_value(std::size_t& seed, const T& value, const Types & ... args) noexcept
+{
+	hash_combine(seed, value);
+	hash_value(seed, args...);
+}
+
+template < typename ... Types >
+std::size_t hash_value(const Types & ... args) noexcept
+{
+	std::size_t seed = 0;
+	hash_value(seed, args...);
+	return seed;
+}
+
+int main()
+{	
+	const auto size = 100000000;
+	const auto length = 10;
+	const auto N = 10000;
+	std::set < std::string > set_of_unique_strings = make_random_words(size, length);
+
+	std::set < std::size_t > collisions;
+	auto num_of_collisions = 0;
+	auto i = 0;
+
+	for (const auto& string : set_of_unique_strings)
+	{
+		auto integral = rand() % N;
+		auto pair = collisions.insert(hash_value(string, integral));
+		auto flag = pair.second;
+		if (!flag)
+			num_of_collisions++;
+		std::cout << integral << " " << string << std::endl;
+		if (!flag)
+			std::cout << i << " " << num_of_collisions << std::endl;
+		++i;
+	}
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
